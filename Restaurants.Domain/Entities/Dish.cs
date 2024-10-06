fc91f8dd6eb4d93af66598d266b68e64ba2d66ce
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-10-06 20:38:18 +0400

    [feat] added DBContext and initial migration

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -6,4 +6,6 @@ public class Dish
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; }
+
+    public Guid RestaurantId { get; set; }
 }
---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext : DbContext
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost;Database=Restaurants;User Id=sa;Password=Abcd@12345;MultipleActiveResultSets=true;;TrustServerCertificate=true;");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
