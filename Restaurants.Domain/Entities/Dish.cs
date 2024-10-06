2fa8a1ec800f66441b9e0ce0a26a0783aee7ef91
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-10-06 10:24:16 +0400

    [feat] added application and domain layer, added restaurant entity

---
+++ Restaurants.Domain/Entities/Dish.cs
@@ -0,0 +1,9 @@
+namespace Restaurants.Domain.Entities;
+
+public class Dish
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
