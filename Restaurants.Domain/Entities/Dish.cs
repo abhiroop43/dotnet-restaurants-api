7ee5821b8f6b44ec94a317ef23da56749e3298b4
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-10-08 00:13:32 +0400

    fix: updated connectionstring

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -6,6 +6,7 @@ public class Dish
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; }
+    public int? KiloCalories { get; set; }
 
     public Guid RestaurantId { get; set; }
 }
