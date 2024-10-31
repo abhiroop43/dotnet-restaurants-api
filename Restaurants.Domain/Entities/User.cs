65c6820ba43497c91f186cb960f2079e67cfdd25
Abhiroop Santra <abhiroop.santra@gmail.com>
2024-10-31 14:36:45 +0400

    feat: added additional user fields

--- Restaurants.Domain/Entities/User.cs
+++ Restaurants.Domain/Entities/User.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Restaurants.Domain.Entities;
 
 public class User : IdentityUser
 {
+    public DateOnly? DateOfBirth { get; set; }
+
+    [MaxLength(50)] public string? Nationality { get; set; }
 }
